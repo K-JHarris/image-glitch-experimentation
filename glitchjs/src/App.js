b55faed75f4a3614d47006f28e9f7428cfc5dbe3
Khari <kharris2k16@gmail.com>
2019-11-13 01:48:43 -0800

    React app created, excess files removed, output confirmed

---
+++ glitchjs/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import MainContainer from './components/mainContainer/mainContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.imgur.com/VS0D97S.jpg"></img>
+        <p>Hello Captain! No errors here. Were you surprised?</p>
+        <MainContainer/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ glitchjs/src/components/mainContainer/mainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function mainContainer() {
+  return (
+    <div className="container">
+      <h1>glitchJS</h1>
+    </div>
+  );
+}
+
+export default mainContainer;
