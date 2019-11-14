023c0af047eb5a9e332b91e7e215181819504114
Khari <kharris2k16@gmail.com>
2019-11-14 04:19:51 -0800

    Still broken

--- glitchjs/src/App.js
+++ glitchjs/src/App.js
@@ -4,9 +4,9 @@ import MainContainer from './components/mainContainer/mainContainer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" id="app-container">
       <header className="App-header">
-        <img src="https://i.imgur.com/VS0D97S.jpg"></img>
+        <img id="image" src="https://i.imgur.com/VS0D97S.jpg"></img>
         
         <p>Hello Captain! No errors here. Were you surprised?</p>
         <MainContainer/>
--- glitchjs/src/components/mainContainer/mainContainer.js
+++ glitchjs/src/components/mainContainer/mainContainer.js
@@ -1,65 +1,53 @@
-import React from 'react';
-import glitch from 'glitch-canvas';
-import srcImage from '../images/lamb.png'
+import React, { Component } from "react";
+import glitch from "glitch-canvas";
+import srcImage from "../images/lamb.png";
+// import ReactDOM from "react-dom";
 
-function MainContainer() {
-  return (
-    <div className="container">
-      <h1>glitchJS</h1>
-    </div>
-  );
+class MainContainer extends Component {
+  state = {
+    glitchImg: '',
+    srcImage: srcImage,
+    params: {
+      amount: 83, //0 - 99
+      iterations: 12, //0 - ~
+      quality: 60, //0 - 99
+      seed: 36 //0 - 99
+    }
+  };
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    console.log('the component mounted.')
+  }
+
+  render() {
+    function loadImage(src, callback) { //declare function that takes in an image source and a callback
+      var imageEl = new Image(); //establish imageEl as a new image in the DOM
+      imageEl.src = src; //set source of newly created image to source argument from function
+      imageEl.onload = function() {//when image successfully loads, run the provided callback on the created image
+        callback(imageEl);
+      };
+    };
+    
+    loadImage(this.state.imagePath, function(img) {//run function passing in image source from state and the following function as a callback to run on the image our function creates
+      glitch(this.state.params) //pass in glitch parameters
+        .fromImage(img) //expects an image from any source, input
+        .toDataURL() //takes no parameters, returns a string with encoded image url
+        .then(function(dataURL) {//run a function that accepts our encoded image as an argument
+          console.log(dataURL);
+          return dataURL;
+          // var imageEl = new Image(); //functionally the same as document.createElement('img')
+          // imageEl.src = dataURL; //set source of newly created image to our encoded url
+        });
+    });
+    return (
+      <div className="container">
+        <h1>glitchJS</h1>
+        <img src={this.state.srcImage}></img>
+        <div id="targetDiv"></div>
+      </div>
+    );
+  }
 }
-
 export default MainContainer;
-
-var imagePath = srcImage;
-//create container reference
-var imgContainerEl = document.getElementById('img-container');
-//set glitch parameters
-var params = {
-	amount:     30,
-	iterations: 10,
-	quality:    30,
-	seed:       25
-};
-
-
-//invoke load image function, passing it imagePath as its source and the following function as its callback
-loadImage(imagePath, function(img) {
-    glitch(params) //pass in glitch parameters
-      .fromImage(img) //expects an image from any source, input
-      .toDataURL()//takes no parameters, returns a string with encoded image url
-        //run a function that accepts our encoded image as an argument  
-        .then( function(dataURL) { 
-          var imageEl = new Image(); //functionally the same as document.createElement('img')
-          imageEl.src = dataURL; //set source of newly created image to our encoded url
-          imgContainerEl.appendChild(imageEl); //append image element into its container
-        } );
-  } 
-);
-
-//create function that creates an image in the DOM, sets its src and passes it to another function in a callback
-function loadImage (src, callback) {
-	var imageEl = new Image();
-  imageEl.src = src;
-	imageEl.onload = function () {
-		callback(imageEl);
-	};
-}
-
-// var image = new Image();
-
-// image.onload = function () {
-// 	glitch()
-// 		.fromImage( image )
-// 		.toDataURL()
-// 		.then( function( dataURL ) {
-// 			var glitchedImg = new Image();
-//       glitchedImg.src = dataURL;
-//       console.log(glitchedImg)
-// 			document.body.appendChild( glitchedImg );
-// 		} );
-// };
-
-
-// image.src= 'images/lamb.png';
